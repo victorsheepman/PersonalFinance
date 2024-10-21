392034cd3e7d80a50ca9a2b498f4ff9fda79e282
Victor Marquez <72099625+victorsheepman@users.noreply.github.com>
2024-10-21 10:17:40 -0400

    Initial Commit

---
+++ PersonalFinance/PersonalFinanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalFinanceApp.swift
+//  PersonalFinance
+//
+//  Created by Victor Marquez on 21/10/24.
+//
+
+import SwiftUI
+
+@main
+struct PersonalFinanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
