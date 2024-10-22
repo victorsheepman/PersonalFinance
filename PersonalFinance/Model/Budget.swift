43a3e5d1e1758c7f02b13584f60ca9013cc0e3f8
Victor Marquez <72099625+victorsheepman@users.noreply.github.com>
2024-10-22 11:34:29 -0400

    clean

--- PersonalFinance/Model/Budget.swift
+++ PersonalFinance/Model/Budget.swift
@@ -8,17 +8,16 @@
 import Foundation
 
 struct Budget: Identifiable {
-    let id = UUID()             // ID único para cada presupuesto
-    let name: String            // Nombre del presupuesto (categoría o propósito)
-    let max: Double             // Presupuesto máximo asignado
-    let spent: Double           // Cantidad ya gastada
+    let id = UUID()
+    let name: String
+    let max: Double
+    let spent: Double
     
-    // Calcula el porcentaje gastado en base al presupuesto máximo
     var percentageSpent: Double {
         return (spent / max) * 100
     }
     
-    // Devuelve si el presupuesto está sobrepasado
+    
     var isOverBudget: Bool {
         return spent > max
     }
