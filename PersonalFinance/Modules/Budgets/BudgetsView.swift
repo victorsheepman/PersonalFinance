b8fe77f3581655ae9dc6a3fbb3ef6092d02b2c39
Victor Marquez <72099625+victorsheepman@users.noreply.github.com>
2024-10-22 13:55:04 -0400

    spending summary done

--- PersonalFinance/Modules/Budgets/BudgetsView.swift
+++ PersonalFinance/Modules/Budgets/BudgetsView.swift
@@ -8,11 +8,30 @@
 import SwiftUI
 
 struct BudgetsView: View {
+    var totalMax: Double {
+        budgetMock.reduce(0) { $0 + $1.max }
+    }
+       
+     
+    var totalSpent: Double {
+        budgetMock.reduce(0) { $0 + $1.spent }
+    }
+    
     var body: some View {
         NavigationStack {
             ZStack {
                 Color("Background")
                     .edgesIgnoringSafeArea(.all)
+                
+                VStack {
+                    
+                   budgetChart
+                    
+                    Spacer()
+                }
+                .padding(.bottom, 100)
+                .padding(.horizontal)
+                .padding(.top, 40)
                 
             }
             .toolbar {
@@ -41,6 +60,54 @@ struct BudgetsView: View {
                 }
             }
         }
+    }
+    
+    var budgetChart: some View {
+        VStack {
+            PieChart(budgets: budgetMock)
+            
+            Text("Spending Summary")
+                .font(.system(size: 20).bold())
+                .padding(.trailing, 148)
+            
+            ForEach(budgetMock) { budget in
+                HStack {
+                    
+                    Circle()
+                        .fill(budget.theme.color)
+                        .frame(width: 10, height: 10)
+                    
+                    Text(budget.category.rawValue)
+                        .font(.caption)
+                        .foregroundStyle(.secondary)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    
+                    
+                    Text("$\(budget.max, specifier: "%.2f")")
+                        .font(.system(size: 14).bold())
+                        .foregroundStyle(.primary)
+                        .frame(width: 80, alignment: .trailing)
+                    
+                    
+                    Text("of $\(totalMax, specifier: "%.2f") limit")
+                        .font(.callout)
+                        .foregroundStyle(.secondary)
+                        .frame(width: 120, alignment: .trailing)
+                }
+                if budget.id != budgetMock.last?.id {
+                    Divider()
+                        .background(Color("Grey-100"))
+                    
+                }
+                
+            }
+            .padding(.top, 10)
+           
+        
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .padding()
+        .background(RoundedRectangle(cornerRadius: 12).fill(.white))
     }
 }
 
