e3952d4da7c17829697c451a43ddf671e24681ff
Victor Marquez <72099625+victorsheepman@users.noreply.github.com>
2024-10-25 10:50:16 -0400

    progress

--- PersonalFinance/Modules/Transaction/Form/TransactionForm.swift
+++ PersonalFinance/Modules/Transaction/Form/TransactionForm.swift
@@ -8,8 +8,14 @@
 import SwiftUI
 
 struct TransactionForm: View {
+    
+    @ObservedObject var viewModel: BudgetViewModel
+    
     @State private var title: String = ""
     @State private var amount: String = ""
+    @State private var date = Date()
+    @State private var selectedBudget: Budget? = nil
+    
     var body: some View {
         Form {
             TextField("Title", text: $title)
@@ -17,10 +23,29 @@ struct TransactionForm: View {
             TextField("Amount", text: $amount)
                 .keyboardType(.decimalPad)
                 .disableAutocorrection(true)
+            
+            Picker("Budget Theme", selection: $selectedBudget) {
+                ForEach(viewModel.budgets) { budget in
+                    Text(budget.category.rawValue) // Muestra el valor del enum como texto
+                        .tag(budget as Budget?) // Asegúrate de asignar el tag correcto
+                }
+            }
+            .pickerStyle(MenuPickerStyle())
+            .onChange(of: selectedBudget) { newBudget in
+                // Aquí puedes manejar cualquier acción al cambiar el presupuesto
+                print("Selected budget: \(String(describing: newBudget))")
+            }
+            
+            DatePicker(
+                "Date",
+                selection: $date,
+                displayedComponents: .date
+            )
+            .datePickerStyle(.graphical)
         }
     }
 }
 
 #Preview {
-    TransactionForm()
+    TransactionForm(viewModel: BudgetViewModel())
 }
