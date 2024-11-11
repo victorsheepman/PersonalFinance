f84a759e7d9af2b509bc41cfc036788e15aef9ec
Victor Marquez <72099625+victorsheepman@users.noreply.github.com>
2024-11-11 10:53:00 -0400

    added new search

--- PersonalFinance/Modules/Transaction/View/TransactionView.swift
+++ PersonalFinance/Modules/Transaction/View/TransactionView.swift
@@ -11,83 +11,56 @@ import SwiftData
 struct TransactionView: View {
     
     @Query(sort: \Transaction.date) var transactions: [Transaction]
-    
-    @State private var search: String = ""
-    
- 
     @StateObject var viewModel: TransactionViewModel = TransactionViewModel()
     
     @State private var isPresented: Bool = false
-    
+    @State private var searchText: String = ""
     @State private var budgetSelected: BudgetCategory?
     
-    var transactionFiltered: [Transaction] {
-        transactions
-            .filter { transaction in
-                search.isEmpty || transaction.title.localizedCaseInsensitiveContains(search)
-            }
-            .filter { transaction in
-                guard let currentBudget = budgetSelected else { return true }
-                return transaction.budget?.category == currentBudget
-            }
-            .sorted { $0.date > $1.date }
-    }
-    
     var body: some View {
-        BackgroundContainer(isPresented: $isPresented, title: "Transactions"){
-            VStack {
-                List {
-                    transactionFilter
-                    transactionList
+        NavigationStack {
+            ZStack {
+                Color("Background")
+                    .edgesIgnoringSafeArea(.all)
+                VStack {
+                    TransactionListView(filterString: searchText)
+                        .searchable(text: $searchText)
                 }
             }
-        }
-        .sheet(isPresented: $isPresented) {
-            transactionFormContainer
-        }
-    }
-    
-    private var transactionFilter: some View {
-        HStack{
-            TextField("Search Transaction", text: $search)
-           
-            Menu {
-                Button("All Transactions") {
-                    budgetSelected = nil
-                }
-                ForEach(BudgetCategory.allCases, id: \.id) { budget in
-                    
-                    Button(budget.rawValue) {
-                        budgetSelected = budget
+            .navigationTitle("Transactions")
+            .sheet(isPresented: $isPresented) {
+                transactionFormContainer
+            }
+            .toolbar {
+                ToolbarItem() {
+                    Menu {
+                        Button("All Transactions") {
+                            budgetSelected = nil
+                        }
+                        ForEach(BudgetCategory.allCases, id: \.id) { budget in
+                            
+                            Button(budget.rawValue) {
+                                budgetSelected = budget
+                            }
+                        }
+                    } label: {
+                        Image(systemName: "line.3.horizontal.decrease.circle.fill")
+                            .foregroundStyle(.black)
                     }
                 }
-            } label: {
-                Image(systemName: "line.3.horizontal.decrease.circle.fill")
-                    .foregroundStyle(.black)
-            }
-        }
-
-    }
-    
-    private var transactionList: some View {
-        ForEach(transactionFiltered, id: \.id) { t in
-            NavigationLink(destination:
-                            TransactionForm(
-                                transactionToEdit: t,
-                                viewModel: viewModel,
-                                isPresented:$isPresented
-                            )
-            ) {
-                TransactionCellView(transaccion: t)
-            }
-        }.onDelete { indexSet in
-            if let index = indexSet.first {
-                let transactionID = transactionFiltered[index].id
-                viewModel.removeTransaction(transactionID)
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button(action: {
+                        isPresented = true
+                    }) {
+                        Image(systemName: "plus")
+                            .foregroundColor(.black)
+                            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+                    }
+                   
+                }
             }
         }
     }
-    
     private var transactionFormContainer: some View {
         VStack {
             HStack(spacing:95) {
@@ -110,6 +83,42 @@ struct TransactionView: View {
         .background(Color("Background"))
     }
     
+}
+
+
+struct TransactionListView: View {
+    @Query(sort: \Transaction.date) var transactions: [Transaction]
+    @State private var isPresented: Bool = false
+    @StateObject var viewModel: TransactionViewModel = TransactionViewModel()
+    
+    init(filterString: String) {
+        let predicate = #Predicate<Transaction> { transaction in
+            transaction.title.localizedStandardContains(filterString)
+            || filterString.isEmpty
+        }
+        _transactions = Query(filter: predicate)
+    }
+    
+    var body: some View {
+        List {
+            ForEach(transactions, id: \.id) { t in
+                NavigationLink(destination:
+                                TransactionForm(
+                                    transactionToEdit: t,
+                                    viewModel: viewModel,
+                                    isPresented:$isPresented
+                                )
+                ) {
+                    TransactionCellView(transaccion: t)
+                }
+            }.onDelete { indexSet in
+                if let index = indexSet.first {
+                    let transactionID = transactions[index].id
+                    viewModel.removeTransaction(transactionID)
+                }
+            }
+        }
+    }
 }
 
 
