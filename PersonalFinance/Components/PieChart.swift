7c13d92a8cd209818968c5440853957cf49e3cab
Victor Marquez <72099625+victorsheepman@users.noreply.github.com>
2024-11-13 13:49:19 -0400

    progress

--- PersonalFinance/Components/PieChart.swift
+++ PersonalFinance/Components/PieChart.swift
@@ -12,6 +12,20 @@ import SwiftData
 struct PieChart: View {
     @Query(sort: \Budget.id) var budgets: [Budget]
     
+    
+    @State private var rawSelectedChartValue: Double? = 0
+    @State private var selectedDay: Date?
+    
+    var selectedBudget: Budget? {
+        guard let rawSelectedChartValue else { return nil }
+        var total = 0.0
+        return budgets.first {
+            total += $0.max
+            return rawSelectedChartValue <= total
+        }
+    }
+    
+    
     var totalMax: Double {
         budgets.reduce(0) { $0 + $1.max }
     }
@@ -27,26 +41,31 @@ struct PieChart: View {
                 angle: .value("Value", budget.max),
                 innerRadius: .ratio(0.618),
                 outerRadius: .inset(10),
-                angularInset: 1.5
+                angularInset: 1
             )
             .foregroundStyle(budget.theme.color)
+            .opacity(selectedBudget?.id == budget.id ? 1.0 : 0.3)
         }
+        .chartAngleSelection(value: $rawSelectedChartValue.animation(.easeInOut))
         .frame(width: 300, height: 300)
         .chartBackground { ChartProxy in
             GeometryReader {  geometry in
                 if let plotFrame = ChartProxy.plotFrame{
                     let frame = geometry[plotFrame]
-                    VStack {
-                       
-                        Text("$\(totalSpent, specifier: "%.2f")")
-                            .font(.title2.bold())
-                            .foregroundStyle(.primary)
-                        Text("of $\(totalMax, specifier: "%.2f") limit")
-                            .font(.callout)
-                            .foregroundStyle(.secondary)
-                        
+                    if let selectedBudget {
+                        VStack {
+                           
+                            Text("$\(selectedBudget.spent, specifier: "%.2f")")
+                                .font(.title2.bold())
+                                .foregroundStyle(.primary)
+                            Text("of $\(selectedBudget.max, specifier: "%.2f") limit")
+                                .font(.callout)
+                                .foregroundStyle(.secondary)
+                            
+                         
+                        }
+                        .position(x: frame.midX, y: frame.midY)
                     }
-                    .position(x: frame.midX, y: frame.midY)
                 }
             }
         }
